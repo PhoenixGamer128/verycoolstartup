46670fbe355c8bc5431e1024a808fea8bdc15d5d
Hugo <hugo4zelda@gmail.com>
2026-02-07 13:14:19 -0700

    create app.jsx and initialize page files

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'styles.css';
+import 'page-styles.css';
+
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Calendar } from './calendar/calendar';
+import { AddEvent } from './addEvent/addEvent';
+import { Compare } from './compare/compare';
+import { About } from './about/about';
+
+export default function App() {
+  return <div className="app">
+    
+  </div>;
+}
---
+++ src/calendar/calendar.jsx
@@ -0,0 +1,7 @@
+export function Calendar() {
+    return (
+        <main>
+            
+        </main>
+    )
+}
