178d9fa640d77326d252fd8dee66dcac08fc3ee4
Hugo <hugo4zelda@gmail.com>
2026-02-09 08:56:45 -0700

    fix file path to image

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,7 +5,7 @@ export function About() {
         <main>
             <div className="basic-box">
                 <h1>About Path for Eternity</h1>
-                <img src="/public/AndHisDisciplesFollowHimCropped.jpg" id="banner" />
+                <img src="/AndHisDisciplesFollowHimCropped.jpg" id="banner" />
                 <p>Path for Eternity was inspired by the Preach My Gospel app for missionaries of The Church of Jesus Christ of Latter-day Saints. I enjoyed using the app to schedule my day and I heard many good things about it from other missionaries. So I decided to make something as similar as possible, simple yet also having the ability to make it as advanced as you want. My hope is that this will continue to be developed after this CS 260 course and will eventually be used by any who may find it useful!</p>
             </div>
         </main>
