17e285207cfa7f7c400e08d4129da7846ee8802b
Hugo <hugo4zelda@gmail.com>
2026-02-14 19:45:07 -0700

    add Calendar reactivity

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import './calendar.css';
+import { RenderEvents } from "./eventDisplay";
 
-export function Calendar() {
+export function Calendar(props) {
+
+
+
     return (
         <main id="calendar-page">
             <div id="calendar-main">
@@ -10,20 +14,7 @@ export function Calendar() {
                     <button className="basic-box">Tasks</button>
                 </div>
                 <div id="event-block">
-                    <div className="event-content basic-box">
-                        <div className="event-time">
-                            <div>8:00</div>
-                            <div>9:00</div>
-                        </div>
-                        <div className="event-info">
-                            <div id="info-detailed">
-                                <div id="event-name">Name</div>
-                                <div id="date">Today</div>
-                            </div>
-                            <div id="event-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a purus orci. Ut mattis mollis lacinia.</div>
-                            <div id="location">@: BYU</div>
-                        </div>
-                    </div>
+                    <RenderEvents username={props.username} />
                 </div>
             </div>
             <div id="taskbar" className="basic-box">
