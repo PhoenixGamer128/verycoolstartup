c2f1c95a04fd2e2462e36704b5dd7867d930fd09
Hugo <hugo4zelda@gmail.com>
2026-02-14 20:35:09 -0700

    add dynamic event styling

--- src/calendar/eventDisplay.jsx
+++ src/calendar/eventDisplay.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { GetEvents } from "../services";
 
+function eventStyle(event) {
+    return {
+        position: "absolute",
+        width: "50%",
+        backgroundColor: event.eventColor,
+        // Recalculate height and top position based on start and end times (assuming times are in "HH:MM" format)
+        height: (event.endTime-event.startTime)/1000/60 + "px", // Convert time difference from milliseconds to minutes and set as height
+        top: event.startTime/1000/60 + "px" // Convert start time from milliseconds to minutes and set as top position
+     };
+}
+
 export function RenderEvents(props) {
     const events = GetEvents(props.username);
     return (
         <div id="event-block">
             {events.map((event, index) => (
-                <div key={index} className="event-content basic-box">
+                <div key={index} style={eventStyle(event)} className="event-content basic-box">
                     <div className="event-time">
                         <div>{event.startTime}</div>
                         <div>{event.endTime}</div>
