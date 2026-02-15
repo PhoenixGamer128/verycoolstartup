5f7e644dd97a648c7229482d7fcac41eb04eaa58
Hugo <hugo4zelda@gmail.com>
2026-02-14 19:44:23 -0700

    move event items to eventDisplay.jsx

---
+++ src/calendar/eventDisplay.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { GetEvents } from "../services";
+
+export function RenderEvents(props) {
+    const events = GetEvents(props.username);
+    return (
+        <div id="event-block">
+            {events.map((event, index) => (
+                <div key={index} className="event-content basic-box">
+                    <div className="event-time">
+                        <div>{event.startTime}</div>
+                        <div>{event.endTime}</div>
+                    </div>
+                    <div className="event-info">
+                        <div id="info-detailed">
+                            <div id="event-name">{event.eventName}</div>
+                            <div id="date">{event.eventDate}</div>
+                        </div>
+                        <div id="event-desc">{event.description}</div>
+                        <div id="location">@: {event.location}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
